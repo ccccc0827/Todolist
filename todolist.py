20bf92f17c9f3411c2204be90619581597c555a7
ccccc0827 <prmelonvk@gmail.com>
2026-04-21 23:42:19 +0800

    Update todolist.py

--- todolist.py
+++ todolist.py
@@ -713,7 +713,7 @@ def render_day_panel(day_name: str, day_date: date, frame: pd.DataFrame, selecte
             st.info("這一天目前沒有任務，可以留白或新增安排。")
         else:
             for row in subset.itertuples():
-                c1, c2, c3 = st.columns([0.5, 0.4, 0.2])
+                c1, c2, c3 = st.columns([0.5, 0.45, 0.2])
 
                 with c1:
                     st.write(row.task_name)
